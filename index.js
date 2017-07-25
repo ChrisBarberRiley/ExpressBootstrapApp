3024703416cbe3567f34f826307227e98bb0e447
Chris Barber-Riley <chris.barberriley@gmail.com>
2017-07-25 20:42:29 +0100

    Moved app to router

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const http        = require('http');
 const bodyParser  = require('body-parser');
 const morgan      = require('morgan');
 const app         = express();
+const router      = require('./router');
 
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
 
-app.get('/', function(req, res){
-  res.send('hello world!');
-});
-
+router(app);
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app)
